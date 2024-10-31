912f96e8a5f10c95accd4fe6b8c593005cafda9f
SHIO777 <shiotatsu1015@gmail.com>
2024-10-31 16:30:53 +0900

    update: motor

--- python/test/test_motor.py
+++ python/test/test_motor.py
@@ -10,25 +10,28 @@ pi = pigpio.pi()
 
 pi.set_mode(MOTOR_PWM, pigpio.OUTPUT)
 pi.set_mode(MOTOR1_IN1, pigpio.OUTPUT)
-pi.set_mode(MOTOR1_IN2, pigpio.OUTPUT)
+# pi.set_mode(MOTOR1_IN2, pigpio.OUTPUT)
 
 pi.write(MOTOR1_IN1, 0)
-pi.write(MOTOR1_IN2, 0)
+# pi.write(MOTOR1_IN2, 0)
 pi.set_PWM_frequency(MOTOR1_IN1, 10000)
 pi.set_PWM_range(MOTOR1_IN1, 100)
 pi.set_PWM_dutycycle(MOTOR1_IN1, 0)
-pi.set_PWM_frequency(MOTOR1_IN2, 10000)
-pi.set_PWM_range(MOTOR1_IN2, 100)
-pi.set_PWM_dutycycle(MOTOR1_IN2, 0)
+
+pi.set_PWM_frequency(MOTOR_PWM, 10000)  # 周波数10kHz
+pi.set_PWM_range(MOTOR_PWM, 100)        # 範囲100
+pi.set_PWM_dutycycle(MOTOR_PWM, 0)
 
 time.sleep(1)
 start_time = time.time()
 
 print("start motor control")
-while (time.time()-start_time < 60):
-    pi.set_PWM_dutycycle(MOTOR_PWM, 100)
-    pi.set_PWM_dutycycle(MOTOR1_IN1, 100)
-    pi.set_PWM_dutycycle(MOTOR1_IN2, 0)
+while (time.time()-start_time < 30):
+    pwm_duty = 100
+    
+    pi.set_PWM_dutycycle(MOTOR_PWM, 100)  # PWMを100%に設定
+    pi.set_PWM_dutycycle(MOTOR1_IN1, pwm_duty)  # IN1にデューティサイクルを設定
+    pi.set_PWM_dutycycle(MOTOR1_IN2, 0)         # IN2を0に設定
     time.sleep(1)
 
 pi.stop()
