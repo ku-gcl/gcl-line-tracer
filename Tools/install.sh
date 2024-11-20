4263265fd960fda25e8554e8a3220411bf3db6c0
Tatsuya Shiotsuka <61105696+SHIO777@users.noreply.github.com>
2024-11-20 20:16:07 +0900

    Update install.sh

--- Tools/install.sh
+++ Tools/install.sh
@@ -2,11 +2,14 @@
 # pigpio
 # cv2
 sudo apt update
+sudo apt upgrade
+
+sudo apt install git
 
 sudo apt install pigpio
 sudo pigpiod
 
-sudo apt install code
+# sudo apt install code
 
 
 # sudo reboot
